a593a3b5dd1a5764c6a36b11af0cf3c22d637c8b
pac <debianunix6@gmail.com>
2022-10-26 16:09:42 +0200

    petit push avec de l'amelioration

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,25 @@ use rand::Rng;
 
 const X_SIZE_MAP: usize = 50;
 const Y_SIZE_MAP: usize = 10;
+const POINT: u32 = 10;
 const WALL_CHAR: char = '#';
 const APPLE_CHAR: char = '@';
-const SNAKE_CHAR: char = '#';
-const SNAKE_TAILS_CHAR: char = '^';
+const SNAKE_CHAR: char = 'O';
+const SNAKE_TAILS_CHAR: char = '¤';
 
 fn main() {
     let mut map: Vec<Vec<char>> = vec![vec![WALL_CHAR; X_SIZE_MAP]; Y_SIZE_MAP];
     let mut is_game_done: bool = false;
     let device_state = DeviceState::new();
     let mut keys: Vec<Keycode>;
+    let mut points: u32 = 0;
 
     let mut snake: Snake = Snake {
         x: X_SIZE_MAP / 2,
         y: Y_SIZE_MAP / 2,
         icon: SNAKE_CHAR,
         tails: vec![],
+        direction: 'Z',
     };
 
     generate_map(&mut map);
@@ -32,97 +35,110 @@ fn main() {
 
     while !is_game_done {
         keys = device_state.get_keys();
+        let last_snake_position_x: usize;
+        let last_snake_position_y: usize;
+
         for key in keys.iter() {
             match key {
                 Keycode::D => {
-                    if map[snake.y][snake.x + 1] != WALL_CHAR {
-                        if map[snake.y][snake.x + 1] == APPLE_CHAR {
-                            snake.tails.push(Tails {
-                                x: snake.x,
-                                y: snake.y,
-                                icon: SNAKE_TAILS_CHAR,
-                            });
-                            place_apple(&mut map);
+                    if snake.direction != 'Q' {
+                        if map[snake.y][snake.x + 1] != WALL_CHAR && map[snake.y][snake.x + 1] != SNAKE_TAILS_CHAR {
+                            snake.direction = 'D';
+                        } else {
+                            is_game_done = true;
                         }
-                        map[snake.y][snake.x] = ' ';
-                        snake.x = snake.x + 1;
-                    } else {
-                        is_game_done = true;
                     }
                 }
                 Keycode::Q => {
-                    if map[snake.y][snake.x - 1] != WALL_CHAR {
-                        if map[snake.y][snake.x - 1] == APPLE_CHAR {
-                            snake.tails.push(Tails {
-                                x: snake.x,
-                                y: snake.y,
-                                icon: SNAKE_TAILS_CHAR,
-                            });
-                            place_apple(&mut map);
+                    if snake.direction != 'D' {
+                        if map[snake.y][snake.x - 1] != WALL_CHAR && map[snake.y][snake.x - 1] != SNAKE_TAILS_CHAR {
+                            snake.direction = 'Q';
+                        } else {
+                            is_game_done = true;
                         }
-                        map[snake.y][snake.x] = ' ';
-                        snake.x = snake.x - 1;
-                    } else {
-                        is_game_done = true;
                     }
                 }
                 Keycode::S => {
-                    if map[snake.y + 1][snake.x] != WALL_CHAR {
-                        if map[snake.y + 1][snake.x] == APPLE_CHAR {
-                            snake.tails.push(Tails {
-                                x: snake.x,
-                                y: snake.y,
-                                icon: SNAKE_TAILS_CHAR,
-                            });
-                            place_apple(&mut map);
+                    if snake.direction != 'Z' {
+                        if map[snake.y + 1][snake.x] != WALL_CHAR && map[snake.y + 1][snake.x] != SNAKE_TAILS_CHAR {
+                            snake.direction = 'S';
+                        } else {
+                            is_game_done = true;
                         }
-                        map[snake.y][snake.x] = ' ';
-                        snake.y = snake.y + 1;
-                    } else {
-                        is_game_done = true;
                     }
                 }
                 Keycode::Z => {
-                    if map[snake.y - 1][snake.x] != WALL_CHAR {
-                        if map[snake.y - 1][snake.x] == APPLE_CHAR {
-                            snake.tails.push(Tails {
-                                x: snake.x,
-                                y: snake.y,
-                                icon: SNAKE_TAILS_CHAR,
-                            });
-                            place_apple(&mut map);
+                    if snake.direction != 'S' {
+                        if map[snake.y - 1][snake.x] != WALL_CHAR && map[snake.y - 1][snake.x] != SNAKE_TAILS_CHAR {
+                            snake.direction = 'Z';
+                        } else {
+                            is_game_done = true;
                         }
-                        if snake.tails.len() > 0 {
-                            let last_x = 0;
-                            let last_y = 0;
-                            for (tail_index, tail) in snake.tails.iter_mut().enumerate() {
-                                match tail_index {
-                                    0 => {
-                                        tail.x = snake.x;
-                                        tail.y = snake.y;
-                                    }
-                                    _ => {
-                                        tail.x = snake.tails[tail_index - 1].x + 1;
-                                        tail.y = snake.tails[tail_index - 1].y;
-                                    }
-                                }
-                            }
-                        }
-                        map[snake.y][snake.x] = ' ';
-                        snake.y = snake.y - 1;
-                    } else {
-                        is_game_done = true;
                     }
                 }
                 _ => {}
             }
-            map[snake.y][snake.x] = snake.icon;
-            if snake.tails.len() > 0 {
-                for tail in &snake.tails {
-                    map[tail.y][tail.x] = SNAKE_TAILS_CHAR;
+        }
+
+        map[snake.y][snake.x] = ' ';
+
+        last_snake_position_x = snake.x;
+        last_snake_position_y = snake.y;
+
+        match snake.direction {
+            'D' => {
+                snake.x += 1;
+            }
+            'Q' => {
+                snake.x -= 1;
+            }
+            'S' => {
+                snake.y += 1;
+            }
+            'Z' => {
+                snake.y -= 1;
+            }
+            _ => {}
+        }
+
+        if map[snake.y][snake.x] == APPLE_CHAR {
+            let _ = &snake.tails.push(Tails {
+                x: snake.x,
+                y: snake.y,
+                icon: SNAKE_TAILS_CHAR,
+            });
+            place_apple(&mut map);
+            points += POINT;
+        }
+
+        if snake.tails.len() > 0 {
+            let mut previous_x = 0;
+            let mut previous_y = 0;
+            let mut old_x;
+            let mut old_y;
+            for (tail_index, tail) in snake.tails.iter_mut().enumerate() {
+                map[tail.y][tail.x] = ' ';
+                match tail_index {
+                    0 => {
+                        previous_x = tail.x;
+                        previous_y = tail.y;
+                        tail.x = last_snake_position_x;
+                        tail.y = last_snake_position_y;
+                    }
+                    _ => {
+                        old_x = tail.x;
+                        old_y = tail.y;
+                        tail.x = previous_x;
+                        tail.y = previous_y;
+                        previous_x = old_x;
+                        previous_y = old_y;
+                    }
                 }
+                map[tail.y][tail.x] = tail.icon;
             }
         }
+        map[snake.y][snake.x] = snake.icon;
+
 
         //Display map
         for column in &map {
@@ -131,9 +147,10 @@ fn main() {
             }
             println!();
         }
-        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+        println!("points: {}", points);
 
-        thread::sleep(Duration::from_millis(200))
+        thread::sleep(Duration::from_millis(100));
+        clearscreen::clear().expect("failed to clear screen");
     };
 }
 
@@ -159,6 +176,7 @@ struct Snake {
     y: usize,
     icon: char,
     tails: Vec<Tails>,
+    direction: char,
 }
 
 struct Tails {
